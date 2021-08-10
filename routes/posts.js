e4b26f619d03b2bcb7249ac377062f5320b5ea51
Ciaran Toner <58338331+Ciaran-io@users.noreply.github.com>
2021-08-10 17:33:12 +0100

    Added post.js file to GET, DELETE, UPDATE posts

---
+++ routes/posts.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+const err = 'CAnt find data';
+
+// Retrieve all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Retrieve specific posts
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Submit a post
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPostpost = await post.save();
+    res.json(savedPostpost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Delete post
+router.delete('/:postId', async (req, res) => {
+  try {
+    const removePost = await Post.remove({ _id: req.params.postId });
+    res.json(removePost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// Delete post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatePost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatePost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
